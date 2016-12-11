e1ac859e5157f516a3a7eb49b6dee378b364c60b
hiraditya <hiraditya@gmail.com>
2016-12-11 12:54:28 -0600

    L2 tests for arrays

--- c/algorithm.search.bench.cpp
+++ c/algorithm.search.bench.cpp
@@ -5,6 +5,7 @@
 #include "test_configs.h"
 #include "test_utils.h"
 
+// Linear search on a sequence
 void BM_search_linear(benchmark::State& state) {
   const unsigned N = state.range(0);
   int a[N];
@@ -17,6 +18,21 @@ void BM_search_linear(benchmark::State& state) {
   state.SetComplexityN(N);
 }
 
-COMPLEXITY_BENCHMARK(BM_search_linear, L1);
+// Binary search on a sequence
+void BM_search_binary(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  int a[N];
+  fillSeq(a, a+N);
+  while (state.KeepRunning()) {
+    // searching for all the elements.
+    for (int i = 0; i < N; ++i)
+      benchmark::DoNotOptimize(std::lower_bound(a, a+N, i));
+  }
+  state.SetComplexityN(N);
+}
 
+COMPLEXITY_BENCHMARK(BM_search_linear, L1);
+COMPLEXITY_BENCHMARK(BM_search_binary, L1);
+COMPLEXITY_BENCHMARK(BM_search_linear, L2);
+COMPLEXITY_BENCHMARK(BM_search_binary, L2);
 BENCHMARK_MAIN()
