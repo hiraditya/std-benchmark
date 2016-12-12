098d23b27f4562105388c347a7dc81250de9a506
Aditya Kumar <aditya.k7@samsung.com>
2016-12-12 15:02:12 -0600

    strcpy, strcat

--- c/string.bench.cpp
+++ c/string.bench.cpp
@@ -20,14 +20,16 @@ void BM_strstr(benchmark::State& state) {
 
 void BM_strcat(benchmark::State& state) {
   const unsigned N = state.range(0);
-  const int s2_sz = N/16;
+  const unsigned s2_sz = N/16;
   char s1[N];
   char s2[s2_sz];
   fillRandomChars(s1, s1+N/2);
   fillRandomChars(s2, s2+s2_sz, false);
   while (state.KeepRunning()) {
     // searching for all the elements.
-    benchmark::DoNotOptimize(strcat(s1, s2));
+    // FIXME: strcat is tricky because it keeps concatenating
+    // until the timer stops, that corrupts memory.
+    //benchmark::DoNotOptimize(strcat(s1, s2));
   }
   state.SetComplexityN(N);
 }
@@ -45,16 +47,31 @@ void BM_strchr(benchmark::State& state) {
   state.SetComplexityN(N);
 }
 
-// Linear search on a sequence
 void BM_strcmp(benchmark::State& state) {
   const unsigned N = state.range(0);
   char s1[N];
   char s2[N/16];
   fillRandomChars(s1, s1+N);
   fillRandomChars(s2, s2+N/16);
+  unsigned i = 0;
   while (state.KeepRunning()) {
-    // searching for all the elements.
-    benchmark::DoNotOptimize(strcmp(s1, s2));
+    // strcmp at varying positions
+    benchmark::DoNotOptimize(strcmp(s1+(i++%N), s2));
+  }
+  state.SetComplexityN(N);
+}
+
+void BM_strcpy(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  char s1[N];
+  char s2[N/16];
+  fillRandomChars(s1, s1+N);
+  fillRandomChars(s2, s2+N/16);
+  unsigned i = 0;
+  while (state.KeepRunning()) {
+    // strcpy at varying positions.
+    const unsigned offset = (i++%N)/2;
+    benchmark::DoNotOptimize(strcpy(s1 + offset, s2));
   }
   state.SetComplexityN(N);
 }
@@ -70,7 +87,9 @@ strncpy - copy part of a string
 strrchr - string scanning operation*/
 
 COMPLEXITY_BENCHMARK(BM_strstr, L1);
-COMPLEXITY_BENCHMARK(BM_strcmp, L1);
+COMPLEXITY_BENCHMARK(BM_strcat, L1);
 COMPLEXITY_BENCHMARK(BM_strchr, L1);
+COMPLEXITY_BENCHMARK(BM_strcmp, L1);
+COMPLEXITY_BENCHMARK(BM_strcpy, L1);
 BENCHMARK_MAIN()
 
