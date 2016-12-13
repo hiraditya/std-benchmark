a63398028fe2c99102775d8f58452f831ad81eaa
Aditya Kumar <aditya.k7@samsung.com>
2016-12-13 17:24:54 -0600

    insert, insert_last

--- cxx/mutators.bench.cpp
+++ cxx/mutators.bench.cpp
@@ -42,13 +42,40 @@ void BM_push_back_vector_reserve(benchmark::State& state) {
   state.SetComplexityN(N);
 }
 
-//std::forward_list<int> does not have push_back, push_back :(
-COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::vector<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::list<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::deque<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::vector<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::list<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::deque<int>, L1);
-COMPLEXITY_BENCHMARK_GEN(BM_push_back_vector_reserve, std::vector<int>, L1);
+template<typename V>
+void BM_insert(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  V v(N, 1);
+  auto val = *v.begin();
+  while (state.KeepRunning()) {
+    v.insert(v.begin(), val);
+  }
+  state.SetComplexityN(N);
+}
 
+template<typename V>
+void BM_insert_last(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  V v(N, 1);
+  auto val = *v.begin();
+  while (state.KeepRunning()) {
+    v.insert(--v.end(), val);
+  }
+  state.SetComplexityN(N);
+}
+
+//std::forward_list<int> does not have push_back, push_back :(
+COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::vector<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::list<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back, std::deque<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::vector<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::list<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back_resize, std::deque<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_push_back_vector_reserve, std::vector<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert, std::vector<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert, std::list<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert, std::deque<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert_last, std::vector<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert_last, std::list<int>, L2);
+COMPLEXITY_BENCHMARK_GEN(BM_insert_last, std::deque<int>, L2);
 BENCHMARK_MAIN()
