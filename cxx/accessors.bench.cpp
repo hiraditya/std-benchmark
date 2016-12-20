19c51c632fd082411b98354481857f651fc3d16d
Aditya Kumar <aditya.k7@samsung.com>
2016-12-20 14:19:23 -0600

    add get and at operator

--- cxx/accessors.bench.cpp
+++ cxx/accessors.bench.cpp
@@ -3,11 +3,42 @@
 #include "test_utils.h"
 
 #include<algorithm>
+#include<deque>
 #include<list>
 #include<vector>
 
-// TODO: get, operator[], find, count, equal_range
+// TODO: get, operator[], find, count, equal_range, erase
 
-//COMPLEXITY_BENCHMARK_GEN(BM_sort_stable, std::vector<int>, MSize);
+template<typename V>
+void BM_advance(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  V v;
+  fill_seq(v);
+  while (state.KeepRunning()) {
+    for (unsigned i = 0; i < N; ++i) {
+      auto it = v.begin();
+      std::advance(it, i);
+      benchmark::DoNotOptimize(it);
+    }
+  }
+  state.SetComplexityN(N);
+}
 
+template<typename V>
+void BM_at(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  V v(N);
+  fill_seq(v);
+  while (state.KeepRunning()) {
+    for (unsigned i = 0; i < N; ++i)
+      benchmark::DoNotOptimize(v.at(i));
+  }
+  state.SetComplexityN(N);
+}
+
+static const int MSize = L1;
+COMPLEXITY_BENCHMARK_GEN(BM_advance, std::vector<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_advance, std::list<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_advance, std::deque<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_at, std::vector<int>, MSize);
 BENCHMARK_MAIN()
