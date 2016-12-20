2cce62d64b9203de47d4bf7a26d98c42095ac407
Aditya Kumar <aditya.k7@samsung.com>
2016-12-20 09:59:02 -0600

    unified all algorithm benchmarks and created another benchmark for accessors

---
+++ cxx/accessors.bench.cpp
@@ -0,0 +1,12 @@
+#include "benchmark/benchmark_api.h"
+#include "test_configs.h"
+#include "test_utils.h"
+
+#include<algorithm>
+#include<list>
+#include<vector>
+
+
+//COMPLEXITY_BENCHMARK_GEN(BM_sort_stable, std::vector<int>, MSize);
+
+BENCHMARK_MAIN()
