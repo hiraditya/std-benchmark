1069366edc37512f545c9772d193560822f00db7
Aditya Kumar <aditya.k7@samsung.com>
2016-12-20 15:59:08 -0600

    find random elements from associative container

--- cxx/accessors.bench.cpp
+++ cxx/accessors.bench.cpp
@@ -4,7 +4,11 @@
 
 #include<algorithm>
 #include<deque>
+#include<map>
 #include<list>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
 #include<vector>
 
 // TODO: get, operator[], find, count, equal_range, erase
@@ -36,9 +40,32 @@ void BM_at(benchmark::State& state) {
   state.SetComplexityN(N);
 }
 
+// Insert random elements
+template<typename V>
+void BM_assoc_find_random(benchmark::State& state) {
+  const unsigned N = state.range(0);
+  using CVT = typename V::value_type;
+  using VT = typename remove_const<CVT>::type;
+  using KT = typename std::remove_const<typename V::key_type>::type;
+  std::vector<KT> temp(N);
+  fill_random(temp, N);
+  V v;
+  for (unsigned i = 0; i < N; ++i)
+    v.insert(get_rand<VT>(RAND_MAX));
+  while (state.KeepRunning()) {
+    for (unsigned i = 0; i < N; ++i)
+      benchmark::DoNotOptimize(v.find(temp[i]));
+  }
+  state.SetComplexityN(N);
+}
+
 static const int MSize = L1;
 COMPLEXITY_BENCHMARK_GEN(BM_advance, std::vector<int>, MSize);
 COMPLEXITY_BENCHMARK_GEN(BM_advance, std::list<int>, MSize);
 COMPLEXITY_BENCHMARK_GEN(BM_advance, std::deque<int>, MSize);
 COMPLEXITY_BENCHMARK_GEN(BM_at, std::vector<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_assoc_find_random, std::set<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_assoc_find_random, std::unordered_set<int>, MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_assoc_find_random, SINGLE_ARG(std::map<int, int>), MSize);
+COMPLEXITY_BENCHMARK_GEN(BM_assoc_find_random, SINGLE_ARG(std::unordered_map<int, int>), MSize);
 BENCHMARK_MAIN()
