335e2d258c9b7c5df161a2a3a0da67f92fe3d18e
hiraditya <hiraditya@gmail.com>
2016-12-23 07:20:16 -0600

    add more random tests

--- test/test_random.cpp
+++ test/test_random.cpp
@@ -1,12 +1,25 @@
 #include "rng_utils.h"
-#include<iostream>
 #include<cassert>
 
-int main() {
-  random_device r;
-  for (int i = 0; i < 10000; i++) {
-    auto v = r.get_rand<float>(-1, 1);
-    assert(v < 1 && v > -1);
+template<typename T>
+void test_rand(random_device &r, T min, T max, int iter = 100) {
+  for (int i = 0; i < iter; i++) {
+    auto v = r.get_rand<T>(min, max);
+    assert(v < max && v > min);
+    assert(v != max && v != min);
   }
 }
 
+template<typename T>
+void rand_tests(random_device &r) {
+  test_rand<T>(r, -1, 1);
+  test_rand<T>(r, 0, 1);
+  test_rand<T>(r, -1, 0);
+  test_rand<T>(r, 0, 100);
+}
+
+int main() {
+  random_device r;
+  rand_tests<float>(r);
+  rand_tests<double>(r);
+}
